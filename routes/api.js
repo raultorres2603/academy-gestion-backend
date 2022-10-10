eb047bc82ba31f55378fb586c6dc293c87e68c0a
Raúl Torres Alarcón <raultorres2603@gmail.com>
2022-10-10 11:39:30 +0200

    Aulas

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var config = require("../bin/config.json");
 var mysql = require("mysql");
 
-
 var connection = mysql.createConnection({
   host: config.host,
   user: config.user,
@@ -13,7 +12,12 @@ var connection = mysql.createConnection({
 
 /* GET home page. */
 router.post("/login", function (req, res, next) {
-  if ((req.body.username && req.body.password) && (req.body.username.length > 5 && req.body.password.length > 5)) {
+  if (
+    req.body.username &&
+    req.body.password &&
+    req.body.username.length > 5 &&
+    req.body.password.length > 5
+  ) {
     connection.query(
       `SELECT COUNT(*) as cont FROM users WHERE username = '${req.body.username.toUpperCase()}'`,
       (err, resu) => {
@@ -23,7 +27,7 @@ router.post("/login", function (req, res, next) {
         } else {
           if (resu[0].cont == 0) {
             connection.query(
-              `INSERT INTO users(username, password) VALUES('${req.body.username.toUpperCase()}','${req.body.password.toUpperCase()}')`,
+              `INSERT INTO users(username, password, type) VALUES('${req.body.username.toUpperCase()}','${req.body.password.toUpperCase()}', 1)`,
               (err, resu) => {
                 if (err) {
                   res.send(JSON.stringify({ error: "3" }));
@@ -46,25 +50,26 @@ router.post("/login", function (req, res, next) {
                   res.send(JSON.stringify({ error: "2" }));
                   console.log(JSON.stringify({ error: "2" }));
                 } else {
-                  if (typeof resu[0] !== 'undefined') {
+                  if (typeof resu[0] !== "undefined") {
                     if (resu[0].idUser > 0) {
                       res.send(
-                        JSON.stringify({ message: "exist", user: resu[0].idUser })
+                        JSON.stringify({
+                          message: "exist",
+                          user: resu[0].idUser,
+                        })
                       );
                       console.log(
-                        JSON.stringify({ message: "exist", user: resu[0].idUser })
+                        JSON.stringify({
+                          message: "exist",
+                          user: resu[0].idUser,
+                        })
                       );
                     } else {
-                      res.send(
-                        JSON.stringify({ error: "4" })
-                      );
+                      res.send(JSON.stringify({ error: "4" }));
                     }
                   } else {
-                    res.send(
-                      JSON.stringify({ error: "4" })
-                    );
+                    res.send(JSON.stringify({ error: "4" }));
                   }
-                  
                 }
               }
             );
@@ -78,27 +83,53 @@ router.post("/login", function (req, res, next) {
 });
 
 router.post("/userInfo", function (req, res, next) {
-  connection.query(`SELECT firstName, idUser, secondName, type, nif, tel, country, city FROM users WHERE idUser = ${req.body.idUser}`, (err, resu) => {
-    if (err) {
-      res.send(JSON.stringify({ error: "1" }));
-      console.log(JSON.stringify({ error: "1" }));
-    } else {
-      res.send(JSON.stringify({ firstName: resu[0].firstName, secondName: resu[0].secondName, type: resu[0].type, nif: resu[0].nif, tel: resu[0].tel, country: resu[0].country, city: resu[0].city }));
-      console.log(JSON.stringify({ firstName: resu[0].firstName, secondName: resu[0].secondName, type: resu[0].type, nif: resu[0].nif, tel: resu[0].tel, country: resu[0].country, city: resu[0].city }));
+  connection.query(
+    `SELECT firstName, idUser, secondName, type, nif, tel, country, city FROM users WHERE idUser = ${req.body.idUser}`,
+    (err, resu) => {
+      if (err) {
+        res.send(JSON.stringify({ error: "1" }));
+        console.log(JSON.stringify({ error: "1" }));
+      } else {
+        res.send(
+          JSON.stringify({
+            firstName: resu[0].firstName,
+            secondName: resu[0].secondName,
+            type: resu[0].type,
+            nif: resu[0].nif,
+            tel: resu[0].tel,
+            country: resu[0].country,
+            city: resu[0].city,
+          })
+        );
+        console.log(
+          JSON.stringify({
+            firstName: resu[0].firstName,
+            secondName: resu[0].secondName,
+            type: resu[0].type,
+            nif: resu[0].nif,
+            tel: resu[0].tel,
+            country: resu[0].country,
+            city: resu[0].city,
+          })
+        );
+      }
     }
-  });
+  );
 });
 
 router.post("/updateUser", function (req, res, next) {
-  connection.query(`UPDATE users SET firstName = '${req.body.firstName}', secondName = '${req.body.secondName}', nif = '${req.body.nif}', tel = '${req.body.tel}', country = '${req.body.country}', city = '${req.body.city}' WHERE idUser = ${req.body.idUser}`, (err, resu) => {
-    if (err) {
-      res.send(JSON.stringify({ error: "1" }));
-      console.log(JSON.stringify({ error: "1" }));
-    } else {
-      res.send(JSON.stringify({ message: "ok" }));
-      console.log(JSON.stringify({ message: "ok" }));
+  connection.query(
+    `UPDATE users SET firstName = '${req.body.firstName}', secondName = '${req.body.secondName}', nif = '${req.body.nif}', tel = '${req.body.tel}', country = '${req.body.country}', city = '${req.body.city}' WHERE idUser = ${req.body.idUser}`,
+    (err, resu) => {
+      if (err) {
+        res.send(JSON.stringify({ error: "1" }));
+        console.log(JSON.stringify({ error: "1" }));
+      } else {
+        res.send(JSON.stringify({ message: "ok" }));
+        console.log(JSON.stringify({ message: "ok" }));
+      }
     }
-  })
+  );
 });
 
 module.exports = router;
--- routes/aulas.js
+++ routes/aulas.js
@@ -11,7 +11,18 @@ var connection = mysql.createConnection({
   database: config.database,
 });
 
-router.post(`/setAulas`, (req, res) => {
+router.post(`/create`, (req, res) => {
+  let name = req.body.name;
+  let plant = req.body.plant;
+  let door = req.body.door;
+
+  connection.query(`INSERT INTO aulas(nombre, planta, puerta) VALUES('${name}', '${plant}', '${door}')`, (err, resu) => {
+    if (err) {
+      return (JSON.stringify({'err': 1}));
+    } else {
+      return (JSON.stringify({'insertID': resu.insertId}));
+    }
+  })
 
 })
 
